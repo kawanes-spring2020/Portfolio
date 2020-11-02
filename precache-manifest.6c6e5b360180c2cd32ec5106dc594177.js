e693129daeae210dcb435dbfb1e2b2189020eb5d
Shubham Kawane <kawane.s@husky.neu.edu>
2020-11-02 00:27:08 -0500

    Updates

---
+++ precache-manifest.6c6e5b360180c2cd32ec5106dc594177.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "2f75fb74723a6ba2fb0d",
+    "url": "/portfolio/static/js/main.2f75fb74.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "2f75fb74723a6ba2fb0d",
+    "url": "/portfolio/static/css/main.572de4dc.chunk.css"
+  },
+  {
+    "revision": "3cd67abe923f0f7d764ebf408d90ac09",
+    "url": "/portfolio/index.html"
+  }
+];
