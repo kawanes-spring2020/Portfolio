e79515be6ad19e4cfb581e99f546c1e981b7cbe6
Shubham Kawane <kawane.s@husky.neu.edu>
2020-11-02 10:55:41 -0500

    Updates

--- precache-manifest.718eb6804a7cec692d12936c822ca8d9.js
+++ precache-manifest.718eb6804a7cec692d12936c822ca8d9.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
   },
   {
-    "revision": "79da61b56df28a5bc25c",
-    "url": "/portfolio/static/js/main.79da61b5.chunk.js"
+    "revision": "020185561a0191f06a8d",
+    "url": "/portfolio/static/js/main.02018556.chunk.js"
   },
   {
     "revision": "5ec21d738fba382f2658",
     "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
   },
   {
-    "revision": "79da61b56df28a5bc25c",
+    "revision": "020185561a0191f06a8d",
     "url": "/portfolio/static/css/main.572de4dc.chunk.css"
   },
   {
-    "revision": "7514cb718cd220c0e171080b03527486",
+    "revision": "ceadb2099d938c370f99e9fd9e85fc2e",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.ae0a3e4fd8aadcb7ede537a9fe45614d.js"
+  "/portfolio/precache-manifest.718eb6804a7cec692d12936c822ca8d9.js"
 );
 
 workbox.clientsClaim();
